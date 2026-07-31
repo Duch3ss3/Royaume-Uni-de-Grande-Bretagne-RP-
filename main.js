cd0f78970fb558023af580c31865289d176ce3c6
Duch3ss3 <thys.nathan.pro@gmail.com>
2026-07-31 16:28:14 +0200

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-const menuButton = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.main-nav');
-if (menuButton && nav) {
-  menuButton.addEventListener('click', () => nav.classList.toggle('open'));
-}
-const sync = document.getElementById('sync-time');
-if (sync) sync.textContent = new Date().toLocaleString('fr-FR');
